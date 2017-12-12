fb495f7e84ae498aa60cd81b40abc461e68a5b44
Zachary Lockner <zalockner@gmail.com>
2017-12-12 13:49:40 -0600

    final commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,100 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
 //initialize game by declaring base variables
 	//target score, users current score, wins, losses
 var targetScore;
 var userScore;
-var wins;
-var losses;
+var wins = 0;
+var losses = 0;
 var crystalValues;
 
-var initializeGame = function() {
+var resetGame = function() {
 	targetScore = Math.floor(Math.random() * 102) + 19;
 	userScore = 0;
-	wins = 0;
-	losses = 0;
 	crystalValues = [];
-}
+};
 
-initializeGame();
+var setGemValues = function () {
+	//if crystalValues.length is 4 or less, keep adding values until it is 4
+	while (crystalValues.length < 4) { // needs to say less than 4 because if it is 3, it will run and add a value, if it is 4, it will stop
+		//var to select random values
+		var randomPoint;
+		var pointRandomizer = function() {
+			randomPoint = Math.floor(Math.random()*12)+1;
+		}
 
-//if crystalValues.length is 4 or less, keep adding values until it is 4
-while (crystalValues.length < 4) { // needs to say less than 4 because if it is 3, it will run and add a value, if it is 4, it will stop
-	//var to select random values
-	var randomPoint;
-	var pointRandomizer = function() {
-		randomPoint = Math.floor(Math.random()*12)+1;
-	}
+		pointRandomizer();
 
-	pointRandomizer();
 
-	//function to check if assigned random point value has been selected already
-
-	for (var i = 0; i <= crystalValues.length; i++) {
-		if (randomPoint === crystalValues[i]) {
-			pointRandomizer();
-		} else {
+		//function to check if assigned random point value has been selected already
+		var repeatBool = false;
+		for (var i = 0; i <= crystalValues.length; i++) {
+			console.log(randomPoint);
+			console.log(crystalValues[i]);
+			if (randomPoint == crystalValues[i]) {
+				repeatBool = false;
+				break;
+			} else {
+				repeatBool = true;
+			}
+		}
+		if (repeatBool === true) {
 			crystalValues.push(randomPoint);
-			break;
 		}
 	}
-}
+};
 
+var setGame = function() {
+	resetGame();
+	setGemValues();
+};
+
+var checkScore = function() {
+	if (userScore === targetScore) {
+		wins++;
+		setGame();
+		$('#win-lose').text("You won!");
+	} else if (userScore > targetScore) {
+		losses++;
+		setGame();
+		$('#win-lose').text("You lost!");
+	}
+};
+
+
+var runGame = function() {
+	console.log(userScore);
+	checkScore();
+	updateDisplay();
+};
+
+ var updateDisplay = function() {
+	$('#target-score').text(targetScore);
+	$('#win-score').text(wins);
+	$('#loss-score').text(losses);
+	$('#user-score').text(userScore);
+};
+
+setGame();
+updateDisplay();
+
+$('#yellow-gem').on("click", function(){
+	userScore = userScore + crystalValues[0];
+	runGame();
+});
+$('#red-gem').on("click", function(){
+	userScore = userScore + crystalValues[1];
+	runGame();
+});
+$('#green-gem').on("click", function(){
+	userScore = userScore + crystalValues[2];
+	runGame();
+});
+$('#blue-gem').on("click", function(){
+	userScore = userScore + crystalValues[3];
+	runGame();
+});
+
+
+});
